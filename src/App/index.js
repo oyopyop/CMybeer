6e5f77d839bbcdb3f36aadad7cdd8ada87920471
Lionel Carre <Lionel.Carre@ext.cdiscount.com>
2022-03-10 15:34:53 +0100

    feat: 🎸 add countdown

---
+++ src/App/index.js
@@ -0,0 +1,5 @@
+import Countdown from "../Countdown";
+
+export default function App() {
+  return <Countdown initialCount={12} />;
+}
---
+++ src/Countdown/index.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import { number } from "prop-types";
+import React from "react";
+
+export default function Countdown({ initialCount = 100, step = 1 }) {
+  const [count, setCount] = React.useState(initialCount);
+
+  const decrement = () => setCount(Math.max(count - step, 0));
+  const reset = () => setCount(initialCount);
+
+  return (
+    <>
+      <p className="countdown-number">{count}</p>
+      <button className="countdown-button" onClick={() => decrement()}>
+        Countdown
+      </button>
+      <button className="countdown-button" onClick={() => reset()}>
+        Reset
+      </button>
+    </>
+  );
+}
+
+Countdown.propTypes = {
+  initialCount: number,
+  step: number,
+};
