58f84d95d9cd83907e82c6ce54c6193256377ff4
Lionel Carre <Lionel.Carre@ext.cdiscount.com>
2022-03-14 13:44:51 +0100

    feat: 🎸 styled-components

--- src/Screen/index.js
+++ src/Screen/index.js
@@ -1,11 +1,8 @@
-import clsx from "clsx";
 import { string, node, bool } from "prop-types";
-import "./styles.css";
+//import "./styles.css";
 import { Title, SubTitle } from "./styles";
 
 export default function Screen({ title, navigation, isHome = false }) {
-  //const subtitleClasse = clsx("subtitle", { homeSubtitle: isHome });
-
   return (
     <>
       <Title>C MY BEER</Title>
