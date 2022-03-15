210d921ba07079cac348d90083f6224a5528af27
Lionel Carre <Lionel.Carre@ext.cdiscount.com>
2022-03-15 14:40:34 +0100

    feat: 🎸 add useBeers

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -1,18 +1,12 @@
-import Screen from "../Screen";
-import Beer from "../Beer";
 import { Grid, CircularProgress } from "@mui/material";
 import React from "react";
 
-const responseToJson = (response) => response.json();
+import useBeers from "../useBeers";
+import Screen from "../Screen";
+import Beer from "../Beer";
 
 export default function HomeScreen() {
-  const [beers, setBeers] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch("https://my-json-server.typicode.com/oyopyop/CMybeer/beers")
-      .then(responseToJson)
-      .then((beers) => setBeers(beers));
-  }, [setBeers]);
+  const [beers] = useBeers();
 
   const hasbeer = beers.length > 0;
 
