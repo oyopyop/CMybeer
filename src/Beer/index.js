8d4ac1b8c11238c096d857f29c43bab8ca828977
Lionel Carre <Lionel.Carre@ext.cdiscount.com>
2022-03-15 11:41:33 +0100

    feat: 🎸 ajout select

--- src/Beer/index.js
+++ src/Beer/index.js
@@ -6,8 +6,21 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+const useQuantity = () => {
+  const [quantity, setQuantity] = React.useState(1);
+
+  const handleChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  return [quantity, handleChange];
+};
 
 export default function Beer({ id }) {
+  const [quantity, setQuantity] = useQuantity();
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -26,8 +39,24 @@ export default function Beer({ id }) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <FormControl variant="standard">
+          <InputLabel id="quantity-label">Quantité</InputLabel>
+          <Select
+            labelId="quantity-label"
+            id="demo-simple-select"
+            value={quantity}
+            label="Quantité"
+            size="small"
+            onChange={setQuantity}
+          >
+            <MenuItem value={1}>1 bouteille</MenuItem>
+            <MenuItem value={2}>2 bouteilles</MenuItem>
+            <MenuItem value={3}>3 bouteilles</MenuItem>
+            <MenuItem value={4}>4 bouteilles</MenuItem>
+            <MenuItem value={5}>5 bouteilles</MenuItem>
+          </Select>
+        </FormControl>
+        <Button size="small">Ajouter</Button>
       </CardActions>
     </Card>
   );
