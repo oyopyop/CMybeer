97a3c624ffda9e4b170032cbae71e0519cacedb8
Lionel Carre <Lionel.Carre@ext.cdiscount.com>
2022-03-21 14:40:36 +0100

    test: 💍 add countdown test

--- src/Countdown/index.js
+++ src/Countdown/index.js
@@ -12,7 +12,14 @@ export default function Countdown({ initialCount = 100, step = 1 }) {
 
   return (
     <>
-      <p className="countdown-number">{count}</p>
+      <p
+        role="timer"
+        aria-label="counter"
+        area-live="polite"
+        className="countdown-number"
+      >
+        {count}
+      </p>
       <button className="countdown-button" onClick={() => decrement()}>
         Countdown
       </button>
