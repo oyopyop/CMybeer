0f2c95b55fdcd04c50aea4de2e1c9931305ff660
Lionel Carre <Lionel.Carre@ext.cdiscount.com>
2022-03-22 11:32:05 +0100

    test: 💍 add countdown stories

---
+++ src/Countdown/index.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Countdown from "./";
+
+export default {
+  title: "Countdown",
+  component: Countdown,
+};
+
+export const Default = () => <Countdown />;
+
+export const WithABigStep = () => <Countdown step={50} />;
+
+export const startAt10 = () => <Countdown initialCount={10} />;
